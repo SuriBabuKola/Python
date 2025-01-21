13e2cd65aae18b81fbb2aac593a98470c4f294da
nagasuribabukola <114101304+nagasuribabukola@users.noreply.github.com>
2025-01-21 05:14:45 +0000

    added int with division, remainder and absolute value

---
+++ Examples/int.py
@@ -0,0 +1,15 @@
+#Integer Variables
+num1 = 10
+num2 = 5
+
+#Integer Division
+result_1 = num1 // num2
+print("Integer Division:", result_1)
+
+#Modulus (Remainder)
+result_2 = num1 % num2
+print("Modulus (Remainder):", result_2)
+
+#Absolute Value
+result_3 = abs(-7)
+print("Absolute Value:", result_3)
