9b6fe85fa8e276ac14884bc9055672387a672752
nagasuribabukola <nagasuribabukola@gmail.com>
2025-01-21 14:53:54 +0530

    added float with add, sub, multi, div and round

---
+++ Examples/float.py
@@ -0,0 +1,20 @@
+#Float Variables
+num1 = 6.0
+num2 = 2.5
+
+#Basic Arithmetic
+result_1 = num1 + num2
+print("Addition:", result_1)
+
+result_2 = num1 - num2
+print("Subtraction:", result_2)
+
+result_3 = num1 * num2
+print("Multiplication:", result_3)
+
+result_4 = num1 / num2
+print("Division:", result_4)
+
+#Rounding
+result_5 = round(5.2194578232, 2) #Rounds to 2 decimal places
+print("Rounded:", result_5)
