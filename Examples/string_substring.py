b255f5b4b94216b420392c87f25d5f1dd95dcad3
nagasuribabukola <114101304+nagasuribabukola@users.noreply.github.com>
2025-01-21 05:10:25 +0000

    added string substring file

---
+++ Examples/string_substring.py
@@ -0,0 +1,7 @@
+text = "Python is Dynamic Programming language"
+substring = "is"
+
+if substring in text:
+  print(substring, "found in the text")
+else:
+  print(substring, "not found in the text")
