ac5677573a39fe57994febf847bf8a164770e2c6
nagasuribabukola <114101304+nagasuribabukola@users.noreply.github.com>
2025-01-22 10:33:35 +0000

    added calculator program using return keyword

---
+++ Examples/new_calculator.py
@@ -0,0 +1,20 @@
+def addition(num1, num2):
+    add = num1 + num2
+    return add
+
+def subtraction(num1, num2):
+    sub = num1 - num2
+    return sub
+
+def multiplication(num1, num2):
+    mul = num1 * num2
+    return mul
+
+def division(num1, num2):
+    div = num1 / num2
+    return div
+
+print("Addition:", addition(20, 5))
+print("Subtraction:", subtraction(10, 5))
+print("Multiplication:", multiplication(5, 15))
+print("Division:", division(50, 5))
