b985020698106b471f41a9aa02a5d4e11eb3172a
nagasuribabukola <114101304+nagasuribabukola@users.noreply.github.com>
2025-01-22 10:32:55 +0000

    added calculator program using functions

---
+++ Examples/calculator.py
@@ -0,0 +1,23 @@
+num1 = 20
+num2 = 5
+
+def addition():
+    add = num1 + num2
+    print("Addition:", add)
+
+def subtraction():
+    sub = num1 - num2
+    print("Subtraction:", sub)
+
+def multiplication():
+    mul = num1 * num2
+    print("Multiplication:", mul)
+
+def division():
+    div = num1 / num2
+    print("Division:", div)
+
+addition()
+subtraction()
+multiplication()
+division()
