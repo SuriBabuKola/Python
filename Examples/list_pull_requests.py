5ca10ab930843f32a239d4b9d72c4e7e58a15b40
nagasuribabukola <114101304+nagasuribabukola@users.noreply.github.com>
2025-01-30 06:18:55 +0000

    added program to list prs in github repo

---
+++ Examples/list_pull_requests.py
@@ -0,0 +1,24 @@
+import requests
+
+url = "https://api.github.com/repos/kubernetes/kubernetes/pulls"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    pull_requests = response.json()
+
+    pr_creators_list = {}
+
+    for pull in pull_requests:
+        creator = pull["user"]["login"]
+        if creator in pr_creators_list:
+            pr_creators_list[creator] += 1
+        else:
+            pr_creators_list[creator] = 1
+    
+    print("PR Creators and Counts: ")
+
+    for creator, count in pr_creators_list.items():
+        print(f"{creator}: {count} PRs")
+else:
+    print("Failed to fetch Data. Status Code: ", response.status_code)
